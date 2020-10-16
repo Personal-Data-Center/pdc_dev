2bb0f781b6268a86e5fb1bebf57aa1a770da23bc
Lucas Coraça Silva <lucascoracasilva@gmail.com>
2020-10-16 13:42:07 -0300

    test

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 setuptools.setup(
     name='pdc_dev',
     packages=setuptools.find_packages(),
-    version='0.1.0',
+    version='0.1.4',
     description='library with pdc funcionalities',
     author='Lucas Coraça Silva',
     url='https://github.com/Personal-Data-Center/pdc_dev',
-    install_requires=['django-cas-ng','Django'],
+    install_requires=['django-cas-ng'],
     license='GPL',
 )
